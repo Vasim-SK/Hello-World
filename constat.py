9c188c0d082ece5ca74c1ddaf0188e464d2a755d
vasim <vasimabdulshaik@gmail.com>
2026-06-13 22:14:04 +0530

    Condition Statement

---
+++ constat.py
@@ -0,0 +1,7 @@
+name = "Vasim"
+age = 22 
+if name == "Vasim" and age == 22:
+    print("Your name is Vasim, and your age is 22")
+
+if name == "Vasim" or name == "Shaik":
+    print("Your name is either Vasim or Shaik")
