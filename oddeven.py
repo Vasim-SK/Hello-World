b9e5e97babf28bc14423889e81cd72b71842135b
vasim <vasimabdulshaik@gmail.com>
2026-06-17 14:02:47 +0530

    For Loop

---
+++ oddeven.py
@@ -0,0 +1,11 @@
+# To print ODD numbers using for loop
+
+for i in range(1, 51):
+    if i % 3 == 0:
+        print(i)
+
+# To print EVEN numbers using for loop
+
+for j in range(1, 50):
+    if j % 2 == 0:
+        print(j)
